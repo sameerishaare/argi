5acedcd3a63cd8ba2d7aea8e05f3a6abf64df6be
Shrived Kakde <104935260+Shrived00@users.noreply.github.com>
2023-11-30 18:16:30 +0530

    Update AboutPage.jsx

--- app/AboutPage.jsx
+++ app/AboutPage.jsx
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 import React from 'react'
-import image1 from '../../public/image1.jpeg'
-import image2 from '../../public/image2.jpeg'
-import image3 from '../../public/image3.jpeg'
+import image1 from '../../public/image1.svg'
+import image2 from '../../public/image2.svg'
+import image3 from '../../public/image3.svg'
 
 const AboutPage = () => {
     return (
         <div className='flex  w-100 flex-col md:flex-row gap-4 m-4
          py-3 justify-center items-center'>
-            <div className="basis-2/5 flex-col justify-center items-center p-4">
+            <div className="basis-2/6 flex-col justify-center items-center p-4">
                 <div className="">
                     <div className="">
                         <Image src={image1} alt="Picture of the author" width={500} style={{ borderRadius: '2%' }} height={500} />
@@ -22,11 +22,11 @@ const AboutPage = () => {
 
 
             </div>
-            <div className="basis-3/5">
-                <div className='mb-2 text-3xl text-green-700'>ABOUT US</div>
-                <div className=' mb-2 font-bold text-3xl text-gray-700'> &quot;Sterling Agrichem Pvt. Ltd. is a Leading producer and global marketer of Agrochemicals, Insecticide, Fungicide, Herbicide, Weedicides, House Hold and Plant Growth Promoter.&quot;</div>
+            <div className="basis-3/6">
+                <div className='mb-2 text-2xl text-green-700'>ABOUT US</div>
+                <div className=' mb-2 font-bold text-3xl text-gray-700'> &quot;Empowering agriculture with precision and innovation,<span className='text-green-700 italic '> Agrity Crop Science </span>pioneers sustainable growth for a flourishing tomorrow.&quot;</div>
 
-                <div className='text-xl text-gray-600'>&quot;Our strategy is to grow through incremental expansion of its existing operations and acquisitions as well as the development, commercialization and marketing of new products and international opportunities. The aim of both our directors is that every product of the company should be manufactured within the company’s own factory and under its own supervision, Theirfor Every product that is sold in our market is made and sold by the company itself.&quot;</div>
+                <div className='text-xl text-gray-600'>&quot;Welcome to Agrity Crop Science, a pioneering force in agropharma. At the intersection of agriculture and pharmaceutical innovation, we cultivate sustainable growth for farmers worldwide. Our commitment to excellence, cutting-edge solutions, and a passion for greener futures define us as leaders in agropharma, nurturing the roots of prosperity in every field we touch.&quot;</div>
             </div>
         </div>
     )
