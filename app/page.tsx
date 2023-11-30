269f5c3b00f8d52e5ec87272a1839298f7d940e0
Shrived Kakde <104935260+Shrived00@users.noreply.github.com>
2023-11-30 18:20:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import Choose from "./Choose"
 import Counter from "./Counter"
-import Exp from "./Exp"
 import OurProduct from "./OurProduct"
 
 export default function Home() {
   return (
     <>
-    <OurProduct/>
-    <Counter/>
-    <Choose/>
-    <Exp/>
+    <AboutPage />
+      <Counter />
+      <OurProduct />
+      <Choose />
+    
 
     </>
   )
