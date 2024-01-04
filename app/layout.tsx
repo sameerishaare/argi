988ea2366d14dacb3e264885980a257d2ff35666
sameerishaare <139305450+sameerishaare@users.noreply.github.com>
2024-01-04 10:40:26 +0530

    more

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Agrity Crop Science',
-  description: "Cultivating Growth: Agropharma's Sustainable Revolution",
+  description: "Agrity Crop Science - Supplier for Bio Insecticide, Pesticides, Herbicides and Plant Growth Promoters for Seed Treatment from Dhoraji, Gujarat, India",
 }
 
 export default function RootLayout({
