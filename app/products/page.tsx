2d9e09b93183ac48fdb2364d0e8af968b2374647
sameerishaare <sameergupta4873@gmail.com>
2024-01-06 10:30:10 +0530

    more

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,11 @@
-"use client"
 import React from 'react'
 import Hero from './Hero'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Agrity crop Science - Products',
+  description: 'Agrity Crop Science - Supplier for Bio Insecticide, Pesticides, Herbicides and Plant Growth Promoters for Seed Treatment from Dhoraji, Gujarat, India',
+};
 
 const page = () => {
   return (
