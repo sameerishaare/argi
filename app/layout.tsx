60b59471bd95acb3eda6ac730148c41623af6246
sameerishaare <sameergupta4873@gmail.com>
2024-01-13 23:33:04 +0530

    adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Agrity Crop Science',
-  description: "Agrity Crop Science - Supplier for Bio Insecticide, Pesticides, Herbicides and Plant Growth Promoters for Seed Treatment from Dhoraji, Gujarat, India",
-}
+  title: "Agrity Crop Science",
+  description:
+    "Agrity Crop Science - Supplier for Bio Insecticide, Pesticides, Herbicides and Plant Growth Promoters for Seed Treatment from Dhoraji, Gujarat, India",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      // google adsense
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4249776601215136"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
